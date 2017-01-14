225152b0acce2baa508ebd80a2cc3656949ffa8e
CCInCharge <charles.l.chen.clc@gmail.com>
2017-01-14 15:43:24 -0800

    Added menu.hpp

---
+++ Project1/menu.hpp
@@ -0,0 +1,18 @@
+/******************************************************************************
+** Program name: menu.hpp
+** Author: Charles Chen
+** Date: 01/14/2017
+** Description:
+
+******************************************************************************/
+
+#include <string>
+
+#ifndef MENU_HPP
+#define MENU_HPP
+
+void printMenu(int rows, int cols, int steps, int startingRow, int startingCol,
+    std::string startingDir)
+void menu();
+
+#endif
