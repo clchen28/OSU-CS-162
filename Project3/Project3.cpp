4d52c8d04e10ff1cb443c5c23fc1cf942c1d3833
CCInCharge <charles.l.chen.clc@gmail.com>
2017-02-11 14:47:44 -0800

    Added documentation to main

--- Project3/Project3.cpp
+++ Project3/Project3.cpp
@@ -3,6 +3,26 @@
 ** Author: Charles Chen
 ** Date: 02/05/2017
 ** Description:
+Project3 is a fantasy fighting simulation game. Various creatures can be
+created, and they can fight against each other. A menu is provided to make this
+simple.
+
+To compile, type:
+make
+
+To run, type:
+./Project3
+
+Each character has attack and defense die, and these are rolled in order to
+determine the outcome of the fight. To test this random element of the program,
+testing utilities have been provided to check that attack die and defense die
+produce expected and reasonably random results.
+
+To compile these test utilities, type:
+make testRolls
+
+To run these test utilities, type:
+./testRolls
 
 ******************************************************************************/
 
